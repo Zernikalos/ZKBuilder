6d3c97aed08c12e8e81acd974f62176a4ec9d775
Aarón <demenus.dev@gmail.com>
2024-01-13 16:50:44 +0000

    feat: 🎸 Fixed attribute indices

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,14 +2,14 @@ import {Attrib, AttribList} from "./Attribs";
 import {Uniform} from "./Uniforms";
 import {ZTypes} from "../zernikalos/ZDataType";
 
-export const ATTR_POSITION: Attrib = new Attrib({name: 'position', shaderName: 'position', three: 'position'})
-export const ATTR_NORMAL: Attrib = new Attrib({name: 'normal', shaderName: 'normal', three: 'normal'})
-export const ATTR_UV: Attrib = new Attrib({name: 'uv', shaderName: 'uv', three: 'uv'})
-export const ATTR_COLOR: Attrib = new Attrib({name: 'color', shaderName: 'color', three: 'color'})
+export const ATTR_INDEX: Attrib = new Attrib({id: 0, name: 'indices', shaderName: '', three: ''})
+export const ATTR_POSITION: Attrib = new Attrib({id: 1, name: 'position', shaderName: 'position', three: 'position'})
+export const ATTR_NORMAL: Attrib = new Attrib({id: 2, name: 'normal', shaderName: 'normal', three: 'normal'})
+export const ATTR_COLOR: Attrib = new Attrib({id: 3, name: 'color', shaderName: 'color', three: 'color'})
+export const ATTR_UV: Attrib = new Attrib({id:4, name: 'uv', shaderName: 'uv', three: 'uv'})
+export const ATTR_BONE_WEIGHT: Attrib = new Attrib({id: 5, name: 'boneWeight', shaderName: 'boneWeight', three: 'skinWeight'})
 
-export const ATTR_BONE_WEIGHT: Attrib = new Attrib({name: 'boneWeight', shaderName: 'boneWeight', three: 'skinWeight'})
-
-export const ATTR_BONE_INDICES: Attrib = new Attrib({name: 'boneIndices', shaderName: 'boneIndices', three: 'skinIndex'})
+export const ATTR_BONE_INDICES: Attrib = new Attrib({id: 6, name: 'boneIndices', shaderName: 'boneIndices', three: 'skinIndex'})
 
 export const ATTRS = new AttribList([
     ATTR_POSITION, ATTR_NORMAL, ATTR_UV, ATTR_COLOR, ATTR_BONE_WEIGHT, ATTR_BONE_INDICES
